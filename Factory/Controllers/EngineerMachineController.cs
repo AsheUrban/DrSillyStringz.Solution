c5d42b7bce4db63b13244ee867a15686bd294086
Ashe Urban <ashe@goldentongue.com>
2022-08-10 13:33:40 -0700

    ADD Index for all machines and engineers.

---
+++ Factory/Controllers/EngineerMachineController.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Factory.Controllers
+{
+  public class EngineerMachineController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public EngineerMachineController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      ViewBag.PageTitle = "View All Engineers and Machines";
+      List<EngineerMachine> model = _db.EngineerMachine.ToList();
+      return View(model);
+    }
+
+    [HttpPost]
+    public ActionResult Create(EngineerMachine engineermachine)
+    {
+      if (_db.EngineerMachine.FirstOrDefault(
+              EM => EM.MachineId == engineermachine.MachineId && 
+                    EM.EngineerId == engineermachine.EngineerId) == null)
+      {
+        _db.EngineerMachine.Add(engineermachine);
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Index");
+    }
+  }
+}
+
